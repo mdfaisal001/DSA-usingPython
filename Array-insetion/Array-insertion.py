e0ce641cccbccaccd8cf053f94a43c2b6cb54578
Mohammed faisal S <108690043+mdfaisal001@users.noreply.github.com>
2024-07-05 22:21:00 +0530

    Update Array-insertion.py

--- Array-insetion/Array-insertion.py
+++ Array-insetion/Array-insertion.py
@@ -3,24 +3,24 @@ import array
 
 def InsertElement(arr,n,pos,x):
     n = int(input("Enter the size of the array"))
-    inputArray = [0] * n 
+    inputArray = [0] * n  # array of [0,0,....] to n time
     print(inputArray)
 
     for i in range(n):
         while True:
             try:
-              inputValue = input(f"Enter the value {i+1} element")
+              inputValue = input(f"Enter the value {i+1} element") #getting input for the input array which contain [0,0,..]
               inputArray[i] = int(inputValue)
               break;
             except ValueError:
                 print("value error")
             
-    x = int(input("Enter the element you want to insert"))
-    pos = int(input("Enter the exact position you want to enter the element"))
-    arr = array.array("i", inputArray)
-    for i in range(n-1,pos-1,-1):
-        arr[i + 1] = arr[i]
-        arr[pos] = x
-        arr.pop()
-        print(arr[pos]) 
-        print(arr)      
+    x = int(input("Enter the element you want to insert"))   #getting input for the element to be insert
+    pos = int(input("Enter the exact position you want to enter the element")) #getting input for the position of the element
+    arr = array.array("i", inputArray) #input array
+    for i in range(n-1,pos-1,-1): #loop start from end 
+        arr[i + 1] = arr[i] #shifting the elements to right
+    arr[pos] = x
+    arr.pop()
+    print(arr[pos]) 
+    print(arr)      
